4755285f148489c6f496b2b71b82499aba364a09
Justissh14 <justissholden14@gmail.com>
2025-06-28 23:04:18 -0400

    Setup Main.cpp and added first reply logic function

---
+++ AiTest/AiTest/AiTest.cpp
@@ -0,0 +1,17 @@
+// AiTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+
+void Reply() {
+	std::string name;
+	std::cin >> name;
+}
+
+int main() {
+	std::cout << "Hey, Welcome to AiTest!" << std::endl;
+	std::cout << "This is a simple test program." << std::endl;
+	Reply();
+	
+}
